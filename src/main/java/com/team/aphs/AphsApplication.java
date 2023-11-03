a5b941017c04be9e1d58a85adadcffbae0bb174f
jiangyk <jiangyk@nwafu.edu.cn>
2023-11-03 22:16:59 +0800

    feat: init project

---
+++ src/main/java/com/team/aphs/AphsApplication.java
@@ -0,0 +1,13 @@
+package com.team.aphs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AphsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AphsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/aphs/AphsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.aphs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AphsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
