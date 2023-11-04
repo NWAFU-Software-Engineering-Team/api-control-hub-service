091140962248a4361e96041e1c332635991b2e36
小新 <782446723@qq.com>
2023-11-04 21:11:58 +0800

    --story=1010515 --user=小新ai 登陆验证服务 https://www.tapd.cn/49591392/s/1035464

---
+++ src/main/java/com/team/aphs/controller/LoginController.java
@@ -0,0 +1,10 @@
+package com.team.aphs.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+    //测试TAPD提交
+}
