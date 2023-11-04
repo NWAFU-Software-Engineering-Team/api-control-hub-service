135adcc178ae6e466f881fce79171aaef8ee2c36
小新 <782446723@qq.com>
2023-11-04 20:48:00 +0800

    规范项目文件结构。

---
+++ src/main/java/com/team/aphs/model/Enity.java
@@ -0,0 +1,5 @@
+package com.team.aphs.model;
+
+public class Enity {
+    //实体类和数据模型类。
+}
---
+++ src/main/java/com/team/aphs/repository/MyRepository.java
@@ -0,0 +1,5 @@
+package com.team.aphs.repository;
+
+public class MyRepository {
+    //数据访问层，用于与数据库交互的接口。
+}
