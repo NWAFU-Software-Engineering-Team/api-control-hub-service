bc467da85eb0670604a8f3f60272abb8768b1788
小新 <782446723@qq.com>
2023-12-04 17:55:43 +0800

    --story=1010844 --user=小新ai 注册账号服务 https://www.tapd.cn/49591392/s/1035869

---
+++ src/main/java/com/team/aphs/controller/RegisterController.java
@@ -0,0 +1,37 @@
+package com.team.aphs.controller;
+
+import com.team.aphs.Dto.RegistrationRequest;
+import com.team.aphs.model.User;
+import com.team.aphs.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+
+    private final UserService userService;
+
+    @Autowired
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseBody
+    public String register(@RequestBody RegistrationRequest registrationRequest) {
+        User user = new User();
+        user.setName(registrationRequest.getUsername());
+        user.setPassword(registrationRequest.getPassword());
+        if (!userService.registerUser(user)) {
+            return "Registration failed";
+        } else {
+            return "Registration successful";
+        }
+    }
+}
+
