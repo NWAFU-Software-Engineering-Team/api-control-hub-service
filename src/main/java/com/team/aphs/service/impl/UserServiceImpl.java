4f7dab12cf5b24dd77b6aae993948158684f6a84
小新 <782446723@qq.com>
2023-12-07 19:00:35 +0800

    feat:实现md5加密算法加密。

--- src/main/java/com/team/aphs/service/impl/UserServiceImpl.java
+++ src/main/java/com/team/aphs/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.team.aphs.model.User;
 import com.team.aphs.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -29,13 +30,28 @@ public class UserServiceImpl implements UserService {
     @Override
     public boolean authenticateUser(String username, String password) {
         User user = userDao.getUserByUsername(username);
-        return user != null && user.getPassword().equals(password);
+
+        if (user != null) {
+            //使用md5解码
+            String passwordWithSalt = password + user.getSalt();
+            String encryptedPassword = DigestUtils.md5DigestAsHex(passwordWithSalt.getBytes());
+            return encryptedPassword.equals(user.getPassword());
+        }
+
+        return false;
     }
 
     @Override
     public boolean registerUser(User user) {
-        // You may want to add additional logic for user validation, checking for existing usernames, etc.
-        userDao.insert(user);
-        return true;
+        // 生成随机盐
+        String salt = String.valueOf(System.currentTimeMillis());
+        user.setSalt(salt);
+
+        // 使用MD5加密密码
+        String passwordWithSalt = user.getPassword() + salt;
+        String encryptedPassword = DigestUtils.md5DigestAsHex(passwordWithSalt.getBytes());
+        user.setPassword(encryptedPassword);
+        int rowsAffected = userDao.insert(user);
+        return rowsAffected > 0;
     }
 }
